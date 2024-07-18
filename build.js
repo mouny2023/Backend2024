948577a45cc9da0d4dcea7bff637eb44ccb4b89a
mouny2023 <zfatima766@gmail.com>
2024-07-18 08:15:15 -0300

    Update build script to use ES module

--- build.js
+++ build.js
@@ -1,6 +1,10 @@
-const ejs = require('ejs');
-const fs = require('fs');
-const path = require('path');
+import ejs from 'ejs';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const templatePath = path.join(__dirname, 'index.ejs');
 const outputPath = path.join(__dirname, 'public', 'index.html');
